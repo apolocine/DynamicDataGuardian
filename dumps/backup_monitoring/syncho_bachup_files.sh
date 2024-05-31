1cd15960df73c0869af1fa588ac16c0e39fb44da
MADANI <101865800+apolocine@users.noreply.github.com>
2024-06-01 00:29:35 +0200

    Update syncho_bachup_files.sh

--- dumps/backup_monitoring/syncho_bachup_files.sh
+++ dumps/backup_monitoring/syncho_bachup_files.sh
@@ -7,4 +7,19 @@
 ## repertoire distant user@remoteHost:/path/to/remote/directory
 ## stdout des erreurs >> /path/to/log/rsync.log 2>&1
 
-* * * * * echo "$(date) - Starting rsync" >> /path/to/log/rsync.log &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@remoteHost:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
+# * * * * * echo "$(date) - Starting rsync" >> /path/to/log/rsync.log &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@remoteHost:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
+
+
+# Définir les variables
+LOG_FILE="/home/hmd/public_html/medical_office/hmd_mesra/sites/mesra.amia.fr/files_test/rsync.log"
+SOURCE_DIR="/home/hmd/public_html/medical_office/hmd_mesra/sites/mesra.amia.fr/files_test"
+DEST_USER="hmd"
+DEST_HOST="amia.fr"
+DEST_DIR="/home/hmd/private_html/medical_office/hmd_mesra/sites/mesra.amia.fr/files_test"
+PASSWORD="hmd"
+
+# Ajouter la date et l'heure dans le fichier de log
+echo "$(date) - Starting rsync" >> $LOG_FILE
+
+# Exécuter la commande rsync avec sshpass pour le mot de passe
+sshpass -p $PASSWORD rsync -avz -e ssh $SOURCE_DIR $DEST_USER@$DEST_HOST:$DEST_DIR >> $LOG_FILE 2>&1
