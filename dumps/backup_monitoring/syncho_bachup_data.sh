f4630b9b4368d8afdd174c60640f3310f61477f8
MADANI <101865800+apolocine@users.noreply.github.com>
2024-06-01 00:18:45 +0200

    Create syncho_bachup_data.sh

---
+++ dumps/backup_monitoring/syncho_bachup_data.sh
@@ -0,0 +1,15 @@
+sudo apt-get install percona-toolkit
+
+#!/bin/bash
+
+# Définir les variables pour les bases de données
+SOURCE_DB="db_dev01"
+TARGET_DB="db_dev02"
+USER="your_mysql_user"
+PASSWORD="your_mysql_password"
+HOST="localhost"
+
+# Synchroniser les tables
+pt-table-sync --execute --verbose h=$HOST,D=$SOURCE_DB,u=$USER,p=$PASSWORD h=$HOST,D=$TARGET_DB,u=$USER,p=$PASSWORD
+
+echo "Synchronisation incrémentielle terminée avec succès."
