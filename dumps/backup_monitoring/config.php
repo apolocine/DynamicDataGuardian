e94036e499e8cf40efe4cf27b23d67e85324ac0d
MADANI <101865800+apolocine@users.noreply.github.com>
2024-05-31 13:31:38 +0200

    Update config.php

--- dumps/backup_monitoring/config.php
+++ dumps/backup_monitoring/config.php
@@ -1,3 +1,6 @@
+//DynamicDataGuardian - Instructions de Configuration
+//Réalisé par Dr Hamid MADANI
+//Le 31/05/2024
 <?php
 // Connexion à la base de données (à personnaliser avec vos propres informations de connexion)
  $servername = "localhost";
