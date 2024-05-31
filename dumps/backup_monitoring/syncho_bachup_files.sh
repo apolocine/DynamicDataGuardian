77fa8bba7e216084e97c8639d9104998869ccbc2
MADANI <101865800+apolocine@users.noreply.github.com>
2024-05-31 21:16:44 +0200

    Update syncho_bachup_files.sh

--- dumps/backup_monitoring/syncho_bachup_files.sh
+++ dumps/backup_monitoring/syncho_bachup_files.sh
@@ -1,2 +1,7 @@
-#!
+#!/bin/bash
+## toutes les minutes * * * * * 
+## marquer la date dans le log echo "$(date) - Starting rsync" >> /path/to/log/rsync.log
+
+ &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@amia.fr:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
+
 * * * * * echo "$(date) - Starting rsync" >> /path/to/log/rsync.log &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@amia.fr:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
