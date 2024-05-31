51d801f80524a3e4784832141bc42956dbcf82ed
MADANI <101865800+apolocine@users.noreply.github.com>
2024-05-31 23:52:29 +0200

    Update syncho_bachup_files.sh

--- dumps/backup_monitoring/syncho_bachup_files.sh
+++ dumps/backup_monitoring/syncho_bachup_files.sh
@@ -4,7 +4,7 @@
 ## && deux commandes successif 
 ## connexion avec password en ssh sshpass -p 'remote_user_password' rsync -avz -e ssh
 ## repertoire local /path/to/remote/directory  
-## repertoire distant hmd@amia.fr:/path/to/remote/directory
+## repertoire distant user@remoteHost:/path/to/remote/directory
 ## stdout des erreurs >> /path/to/log/rsync.log 2>&1
 
-* * * * * echo "$(date) - Starting rsync" >> /path/to/log/rsync.log &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@amia.fr:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
+* * * * * echo "$(date) - Starting rsync" >> /path/to/log/rsync.log &&  sshpass -p 'remote_user_password' rsync -avz -e ssh /path/to/remote/directory  hmd@remoteHost:/path/to/remote/directory >> /path/to/log/rsync.log 2>&1
