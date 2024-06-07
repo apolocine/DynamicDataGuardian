26ec9c93ca866e2c1173bca9c80c605fd7ee2e27
MADANI <101865800+apolocine@users.noreply.github.com>
2024-06-07 11:03:39 +0200

    Update config.sh

--- dumps/backup_monitoring/config.sh
+++ dumps/backup_monitoring/config.sh
@@ -1,18 +1,22 @@
 # config.sh
 
+
+# Réalisé par Dr Hamid MADANI
+# Le 31/05/2024
+
 # Configuration
 DB_NAME="cm_drupal_amia_prod"
-DB_USER="htmc"
-DB_PASS="gjetfm26"
+DB_USER="local_user_name"
+DB_PASS="*****"
 BACKUP_DIR="/home/hmd/dumps"
 BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_$(date +\%Y\%m\%d\%H\%M\%S).sql"
 
-REMOTE_USER="hmd"
-REMOTE_HOST="amia.fr"
+REMOTE_USER="remot_user_name"
+REMOTE_HOST="remote_host_name.fr"
 REMOTE_DIR="/home/hmd/dumps"
-REMOTE_PASS="hmd"
-REMOTE_MYSQL_USER="htmc"
-REMOTE_MYSQL_PASS="gjetfm26"
+REMOTE_PASS="remot_pass"
+REMOTE_MYSQL_USER="user"
+REMOTE_MYSQL_PASS="*****"
 REMOTE_MYSQL_DB="project_backup_monitoring"
 REMOTE_MYSQL_PROJECT_DB_NAME="cm_drupal_amia_hmd_prod"
 PROJECT_NAME="DynamicDataGuardian"
